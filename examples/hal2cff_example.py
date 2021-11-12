23addaa260ea64d7855ccea4be1988e392d4bf89
Romain PRIMET <romain.primet@inria.fr>
2021-11-12 12:15:34 +0100

    still drafting

--- examples/hal2cff_example.py
+++ examples/hal2cff_example.py
@@ -12,14 +12,19 @@ def hal2cff(halref):
 
 def get_hal_graph(halref):
     """
+    halref: str
+        HAL document URL or identifier
     """
     g = Graph()
     g.parse(halref)
     return g
 
 
-def halref_to_url(halref):
+def halref_to_rdf_url(halref):
     """
+    halref: str
+        HAL document URL or identifier
+    
     https://data.archives-ouvertes.fr/document/hal-02371715v2.rdf -> https://data.archives-ouvertes.fr/document/hal-02371715v2.rdf
     https://data.archives-ouvertes.fr/document/hal-02371715 -> https://data.archives-ouvertes.fr/document/hal-02371715.rdf
     https://data.archives-ouvertes.fr/document/hal-02371715v2.json -> https://data.archives-ouvertes.fr/document/hal-02371715v2.rdf
@@ -29,14 +34,38 @@ def halref_to_url(halref):
         pass
 
 
-def get_latest_version(doc):
+# +
+def rdf_to_canonical(ref):
+    assert str(ref).endswith('.rdf')
+    return URIRef(str(ref)[:-4])
+
+def canonical_to_rdf(ref):
+    return URIRef(f"{str(ref)}.rdf")
+
+
+# -
+
+rdf_to_canonical(URIRef("https://data.archives-ouvertes.fr/document/hal-02371715v2.rdf"))
+
+
+def get_latest_version(doc_graph, doc_uri):
     """
-    doc: Graph
+    doc_graph: Graph
+    doc_uri: URIRef or str
     """
-    pass
+    doc_uri = URIRef(doc_uri)
+    # if the document contains a title, return 'doc_uri'.
+    # otherwise, see if doc_uri is in one or more 'has_version' relationships,
+    # and return the URI of the document whose version seems the highest
+    if get_title(doc_graph, doc_uri) is not None:
+        return doc_uri
+    else:
+        print("oh noes")
 
 
 g = get_hal_graph("https://data.archives-ouvertes.fr/document/hal-02371715v2.rdf")
+
+get_latest_version(g, "https://data.archives-ouvertes.fr/document/hal-02371715v2.rdf")
 
 for (sub, obj, pred) in g:
     print(sub,obj,pred)
@@ -56,13 +85,10 @@ def get_abstract(doc_graph, doc_uri):
     return get_attribute(doc_graph, doc_uri, URIRef("http://purl.org/dc/terms/abstract"))
 
 
-get_abstract(g, URIRef("https://data.archives-ouvertes.fr/document/hal-02371715v2"))
+get_abstract(g, URIRef("https://data.archives-ouvertes.fr/document/hal-02371715"))
 
 
-def get_title(doc_graph):
-    return doc_graph.value(URIRef("https://data.archives-ouvertes.fr/document/hal-02371715v2"), URIRef("http://purl.org/dc/terms/title")).value
+def get_title(doc_graph, doc_uri):
+    return get_attribute(doc_graph, doc_uri, URIRef("http://purl.org/dc/terms/title"))
 
 
-get_title(g)
-
-g.identifier
