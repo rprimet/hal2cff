ab85a5d7eadc4d00ee7c0afbbc27d628748cce06
Romain PRIMET <romain.primet@inria.fr>
2021-11-14 12:45:49 +0100

    reordering + refactorings

--- examples/hal2cff_example.py
+++ examples/hal2cff_example.py
@@ -4,6 +4,7 @@ from rdflib import Graph, RDF, URIRef
 import rdflib
 import yaml
 from urllib.parse import urlparse, urlunparse
+from dataclasses import dataclass
 
 
 # For the first step, we want to get
@@ -18,7 +19,11 @@ from urllib.parse import urlparse, urlunparse
 #
 # In a fourth (and last?) step, we want to add (draft) affiliation data to the authors list
 #
-# Extra credits : tools/UIs to wrangle affiliation data, first/other authors distinction, keywords, ...
+# Extra credits: tools/UIs to wrangle affiliation data, first/other authors distinction, keywords, ...
+#
+# Stuff that would be nice but not trivial to do (and it's irksome): "copy to clipboard" button, urlparams...
+#
+# Point to make: really nice for projects that require a server side but no (writable) DB 
 
 def get_hal_graph(halref) -> Graph:
     """
@@ -43,7 +48,7 @@ def halref_to_data_url(halref) -> URIRef:
     assert "hal-" in parsed_ref.path, "Expected HAL (or HAL-data) document URL"
     if "hal.archives-ouvertes.fr" in parsed_ref.netloc:
         parsed_ref = parsed_ref._replace(netloc="data.archives-ouvertes.fr",
-                                        path=f"/document{parsed_ref.path}")
+                                         path=f"/document{parsed_ref.path}")
     return urlunparse(parsed_ref)
 
 
@@ -56,6 +61,7 @@ halref_to_data_url("https://hal.archives-ouvertes.fr/hal-02371715v2")
 def to_canonical(halref) -> URIRef:
     """
     halref: URIRef or str
+        HAL document URI
     """
     if str(halref).endswith('.rdf'):
         return URIRef(str(halref)[:-4])
@@ -65,6 +71,7 @@ def to_canonical(halref) -> URIRef:
 def to_rdf(halref) -> URIRef:
     """
     halref: URIRef or str
+        HAL document URI
     """
     if not str(halref).endswith('.rdf'):
         return URIRef(f"{str(halref)}.rdf")
@@ -82,43 +89,7 @@ to_rdf(URIRef("https://data.archives-ouvertes.fr/document/hal-02371715v2"))
 
 to_rdf(URIRef("https://data.archives-ouvertes.fr/document/hal-02371715v2.rdf"))
 
-
-def get_one_version(doc_graph, doc_uri) -> URIRef:  # XXX change name, refactor
-    """
-    doc_graph: Graph
-    doc_uri: URIRef or str
-    """
-    doc_uri = URIRef(doc_uri)
-    # if the document contains a title, return 'doc_uri'.
-    # otherwise, see if doc_uri is in one or more 'has_version' relationships,
-    # and return the URI of the document whose version seems the highest
-    
-    # get latest version by walking isReplacedBy links
-    if get_title(doc_graph, doc_uri) is not None:
-        return doc_uri
-    else:
-        doc_versions = list(doc_graph.objects(to_canonical(doc_uri), URIRef("http://purl.org/dc/terms/hasVersion")))
-        assert doc_versions, "no version found"
-        return doc_versions[0]
-
-
-def get_latest_version(doc_uri) -> URIRef:
-    """
-    Get the latest version of a document by following 'isReplacedBy' links
-    """
-    doc_graph = Graph().parse(to_rdf(doc_uri))
-    while (replaced_by := get_attribute(doc_graph, doc_uri, "http://purl.org/dc/terms/isReplacedBy")) is not None:
-        doc_uri = replaced_by
-        doc_graph = Graph()
-        doc_graph.parse(to_rdf(doc_uri))
-    return doc_uri
-
-
 g = get_hal_graph("https://data.archives-ouvertes.fr/document/hal-02371715.rdf")
-
-get_one_version(g, "https://data.archives-ouvertes.fr/document/hal-02371715")
-
-get_latest_version("https://data.archives-ouvertes.fr/document/hal-02371715v1")
 
 
 def get_author_nodes(doc_graph: Graph):
@@ -152,6 +123,57 @@ def get_title(doc_graph, doc_uri):
     return get_attribute(doc_graph, doc_uri, URIRef("http://purl.org/dc/terms/title"))
 
 
+def get_one_version(doc_graph, doc_uri) -> URIRef:  # XXX change name, refactor
+    """
+    doc_graph: Graph
+    doc_uri: URIRef or str
+    """
+    doc_uri = URIRef(doc_uri)
+    # if the document contains a title, return 'doc_uri'.
+    # otherwise, see if doc_uri is in one or more 'has_version' relationships,
+    # and return the URI of the document whose version seems the highest
+    
+    # get latest version by walking isReplacedBy links
+    if get_title(doc_graph, doc_uri) is not None:
+        return doc_uri
+    else:
+        doc_versions = list(doc_graph.objects(to_canonical(doc_uri), URIRef("http://purl.org/dc/terms/hasVersion")))
+        assert doc_versions, "no version found"
+        return doc_versions[0]
+
+
+def get_latest_version(doc_uri) -> URIRef:
+    """
+    Get the latest version of a document by following 'isReplacedBy' links
+    """
+    doc_graph = Graph().parse(to_rdf(doc_uri))
+    while (replaced_by := get_attribute(doc_graph, doc_uri, "http://purl.org/dc/terms/isReplacedBy")) is not None:
+        doc_uri = replaced_by
+        doc_graph = Graph()
+        doc_graph.parse(to_rdf(doc_uri))
+    return doc_uri
+
+
+get_one_version(g, "https://data.archives-ouvertes.fr/document/hal-02371715")
+
+get_latest_version("https://data.archives-ouvertes.fr/document/hal-02371715v1")
+
+
+# +
+@dataclass
+class HALAuthor:
+    firstName: str
+    familyName: str
+
+@dataclass
+class HALDocument:
+    title: str
+    abstract: str
+    authors: list[HALAuthor]
+
+
+# -
+
 def hal2cff(halref):
     """
     halref: str
@@ -163,8 +185,8 @@ def hal2cff(halref):
     one_version_halref = get_one_version(graph, halref)
     latest_version = get_latest_version(one_version_halref)
     graph = get_hal_graph(latest_version)  # XXX do not reuse name!
-    title = get_title(graph, latest_version)
-    abstract = get_abstract(graph, latest_version)
+    title = get_title(graph, latest_version).value  # XXX None case
+    abstract = get_abstract(graph, latest_version).value  # XXX None case
     
     # XXX refactor!
     authors = []
@@ -173,29 +195,25 @@ def hal2cff(halref):
         author_graph = get_hal_graph(author_doc_ref)
         fname = get_attribute(author_graph, author_doc_ref, "http://xmlns.com/foaf/0.1/firstName")
         lname = get_attribute(author_graph, author_doc_ref, "http://xmlns.com/foaf/0.1/familyName")
-        authors.append((fname.value, lname.value))
+        authors.append(HALAuthor(firstName=fname.value, familyName=lname.value))
     
-    return title, abstract, authors
+    return HALDocument(title=title, abstract=abstract, authors=authors)
 
 
 model = hal2cff("https://hal.archives-ouvertes.fr/hal-02371715")
+model = hal2cff("https://hal.archives-ouvertes.fr/hal-01361430v1")
 
 model
 
 
-def output_cff(model):
+def output_cff(doc: HALDocument):
     return yaml.dump({
         'cff-version': '1.2.0',
         'message': "If you use this software, please cite both the article from preferred-citation and the software itself.",
         'preferred-citation': {
-            'title': model[0].value,
-            'abstract': model[1].value,
-            'authors': [
-                {
-                    'given-names': author[0],
-                    'family-names': author[1]
-                }
-            for author in model[2:]]
+            'title': doc.title,
+            'abstract': doc.abstract,
+            'authors': doc.authors
         }
     })
 
