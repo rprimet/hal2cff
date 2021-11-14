9b56ad1a1c9912a21fd5e96b54096d0c0f2e0a7c
Romain PRIMET <romain.primet@inria.fr>
2021-11-14 17:12:01 +0100

    reorganize

--- examples/basic_ui.py
+++ examples/basic_ui.py
@@ -9,6 +9,20 @@ from hal2cff import hal2cff
 #
 # Turn a [HAL](https://hal.archives-ouvertes.fr) URL into a draft [CITATION.cff](https://citation-file-format.github.io/) file.
 #
+# In a fourth (and last?) step, we want to add (draft) affiliation data to the authors list
+#
+# Extra credits: 
+# - download button
+# - tools/UIs to wrangle affiliation data, 
+# - first/other authors distinction, 
+# - keywords, 
+# - search docs with the HAL API directly,
+# - et caetera
+#
+# Stuff that would be nice but not trivial to do (and it's irksome): "copy to clipboard" button, urlparams...
+#
+# Point to make: really nice for projects that require a server side but no (writable) DB 
+#
 
 url = widgets.Textarea(value="https://hal.archives-ouvertes.fr/hal-02485642v2")
 button = widgets.Button(description="Generate CFF")
--- examples/hal2cff_example.py
+++ examples/hal2cff_example.py
@@ -2,27 +2,5 @@ from hal2cff import hal2cff
 
 # # hal2cff example
 
-# For the first step, we want to get
-# - title
-# - abstract
-#
-# from a HAL reference.
-#
-# In a second step, we want to format those as a YAML snippet as a valid, preferred-citation CFF file.
-#
-# In a third step, we want to enrich this with the authors list
-#
-# In a fourth (and last?) step, we want to add (draft) affiliation data to the authors list
-#
-# Extra credits: 
-# - tools/UIs to wrangle affiliation data, 
-# - first/other authors distinction, 
-# - keywords, 
-# - search docs with the HAL API directly,
-# - et caetera
-#
-# Stuff that would be nice but not trivial to do (and it's irksome): "copy to clipboard" button, urlparams...
-#
-# Point to make: really nice for projects that require a server side but no (writable) DB 
 
 print(hal2cff("https://hal.archives-ouvertes.fr/hal-01361430v1"))
