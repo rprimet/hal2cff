d1ee10c42ed59b50ecbce6fd590a3a160a5a7ba1
Romain PRIMET <romain.primet@inria.fr>
2021-11-14 13:31:54 +0100

    test cases

--- src/hal2cff/tests/test_hal2cff.py
+++ src/hal2cff/tests/test_hal2cff.py
@@ -3,3 +3,5 @@ from hal2cff._hal2cff import get_hal_graph, halref_to_data_url
 
 def test_halref_to_data_url():
     assert halref_to_data_url("https://hal.archives-ouvertes.fr/hal-02371715v2") == "https://data.archives-ouvertes.fr/document/hal-02371715v2"
+    assert halref_to_data_url("https://data.archives-ouvertes.fr/document/hal-02371715v2.rdf") == "https://data.archives-ouvertes.fr/document/hal-02371715v2.rdf"
+    assert halref_to_data_url("https://data.archives-ouvertes.fr/document/hal-02371715v2") == "https://data.archives-ouvertes.fr/document/hal-02371715v2"
