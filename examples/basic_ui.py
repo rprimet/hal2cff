a2fdde2154ffcdef1ac3c83d11323748125404c6
Romain PRIMET <romain.primet@inria.fr>
2021-11-17 22:28:04 +0100

    add spinner to basic UI

--- examples/basic_ui.py
+++ examples/basic_ui.py
@@ -11,6 +11,9 @@ from hal2cff import hal2cff
 
 url = widgets.Textarea(value="https://hal.archives-ouvertes.fr/hal-02485642v2")
 button = widgets.Button(description="Generate CFF")
+spinner = widgets.HTML(value="""
+<img width="64" alt="Loading icon cropped" src="https://upload.wikimedia.org/wikipedia/commons/9/92/Loading_icon_cropped.gif">
+""", layout=widgets.Layout(visibility="hidden"))
 display(widgets.HBox([url, button]))
 output = widgets.Output()
 display(output)
@@ -19,9 +22,13 @@ display(output)
 def generate_cff(_):
     with output:
         output.clear_output()
-        result = hal2cff(url.value)
-        result_pre = f"<pre>{result}</pre>"
-        display_html(result_pre, raw=True)
+        spinner.layout = widgets.Layout(visibility="visible")
+        try:
+            result = hal2cff(url.value)
+            result_pre = f"<pre>{result}</pre>"
+            display_html(result_pre, raw=True)
+        finally:
+            spinner.layout = widgets.Layout(visibility="hidden")
 
 
 button.on_click(generate_cff)
